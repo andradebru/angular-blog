16a38c7f8eb7fb466f9ec03f82cbfb5dee8667fe
andradebru <brunansandrade@gmail.com>
2023-09-19 23:27:47 -0300

    new style and contents

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,13 +1,19 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "FILME DO ANO",
+    "description": "Barbie é o primeiro filme dirigido por uma mulher a arrecadar 1 bilhão",
+    "photoCover":"https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2023/07/230404125917-09-barbie-movie.jpg?w=876&h=484&crop=1"
   },
   {
     "id":"2",
     "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Title",
     "description": "bla blabla",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   }
