3817ea0392a9c63d623bb7f7ffd8c6e34a0db9a3
andradebru <brunansandrade@gmail.com>
2023-09-22 00:07:15 -0300

    fix content styles

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,18 +3,21 @@ export const dataFake = [
     "id":"1",
     "title": "FILME DO ANO",
     "description": "Barbie é o primeiro filme dirigido por uma mulher a arrecadar 1 bilhão",
-    "photoCover":"https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2023/07/230404125917-09-barbie-movie.jpg?w=876&h=484&crop=1"
+    "photoCover":"https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2023/07/230404125917-09-barbie-movie.jpg?w=876&h=484&crop=1",
+    "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Mariana Enriquez e os fantasmas da Argentina",
+    "description": "Os horrores do passado não tão distante que refletem no presente",
+    "photoCover":"https://s2-galileu.glbimg.com/0PW9MET4sixSbJXuSwi0kSQnx0E=/0x0:4417x3169/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2023/v/t/OAq1ZoTUmB1NiyhEgRqA/mariana-enriquez-sebastian-freire.jpg",
+    "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
   {
     "id":"3",
-    "title": "Title",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "Novo filme do pantera negra lançado em breve",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   }
 ]
